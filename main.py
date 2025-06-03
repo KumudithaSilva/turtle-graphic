0ae0d576d8ea408aa423b9fec1eb3f0db5f8cad9
Kumuditha <kumudithasilva66@gmail.com>
2025-06-03 16:27:53 +0530

    update polygons details and add random walk

--- main.py
+++ main.py
@@ -1,30 +1,44 @@
-import turtle
+import random
+import turtle as t
 from turtle import Turtle, Screen
-from polygons import polygons
+from polygons import turtle_direction
 
-data = polygons
-
+t.colormode(255)
 timmy = Turtle()
 timmy.shape("classic")
 timmy.home()
-timmy.speed(1)
 
-def draw_shape(polygons_data):
-    for i in range(len(polygons_data)):
-         sides = data[i]['sides']
-         central_angle = data[i]['central_angle']
-         color = data[i]['color']
-
-         timmy.color(color)
-         for _ in range(sides):
-             timmy.right(central_angle)
-             timmy.forward(100)
-
-draw_shape(data)
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    return r, g, b
 
 
+def draw_shape():
+    for shape in range(3, 11):
+        central_angle = 360 / shape
+        timmy.color(random_color())
+        for _ in range(shape):
+            timmy.forward(100)
+            timmy.right(central_angle)
 
 
+def random_walk():
+    count = 0
+    timmy.speed(0)
+    timmy.pensize(9)
+    timmy.forward(25)
+
+    while count < 1000:
+        random_path = random.randint(0, 3)
+        timmy.color(random_color())
+        timmy.setheading(turtle_direction[random_path])
+        timmy.forward(25)
+
+        count += 1
+
+random_walk()
 
 screen = Screen()
 screen.exitonclick()
